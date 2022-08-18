8811dc0ba25b2c471a62c2233e895499be4f65ce
Samet Taşcı <samet.tasci@hotmail.com>
2022-08-19 01:00:24 +0300

    9.Gün

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,9 +9,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EfCarDal());
+            //CarTest();
+            //BrandTest();
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
+        }
 
-            
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName);
+            }
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/RentACarProjectontext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarProjectontext.cs
@@ -12,7 +12,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB;Database=RentACarProject;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server = DESKTOP-PF2JRQD;Database=CarRental;Trusted_Connection=true");
         }
 
         public DbSet<Car> Cars { get; set; }
